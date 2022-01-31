cd314b669d11fd8d96ac9c56f3747552e52e364b
sshanbhag09 <69388704+sshanbhag09@users.noreply.github.com>
2022-01-31 19:31:56 +0530

    Updates on main.py

--- main.py
+++ main.py
@@ -1,71 +1,82 @@
-from typing import Optional, Any
-
-BACKGROUND_COLOR = "#B1DDC6"
+THEME_COLOR = "#375362"
+FONT=("Arial",15,"italic")
 from tkinter import *
-import pandas as pd
-import random
-selected={}
-generate_word={}
-try:
-    generate_word=pd.read_csv("data/words_to_learn.csv")
-except FileNotFoundError:
-    generate_word=pd.read_csv("data/french_words.csv")
-else:
-    generate_word=generate_word.to_dict(orient="records")
+
+from quiz_brain import QuizBrain
 
 
-def randomwords():
-    # with open("data/french_words.csv","r") as generate_word:
-    #     random_flines=generate_word.read().splitlines()
-    #     return list(random.choice(random_flines).split(","))
-    global selected,flip_timer
-    window.after_cancel(flip_timer)
+class Quizinterface:
+    def __init__(self,quizbrain:QuizBrain):
+        self.window= Tk()
+        self.score=0
+        self.quiz=quizbrain
 
-    selected=random.choice(generate_word)
+        self.set()
+        self.label=Label(text=f"Score: {self.score}",bg=THEME_COLOR,fg="white",pady=20)
+        self.label.grid(row=0,column=1)
+        # self.menu = StringVar()
+        # self.menu.set("Any")
+        # self.drop= OptionMenu(self.window,self.menu,"Any","Entertainment:Films","Science & Nature","General knowledge")
+        # self.drop.grid(row=0,column=0)
+        # self.bsub=Button(highlightthickness=0,command=self.chosenCategory)
+        # self.bsub.grid(row=3,column=0)
+        self.canvas = Canvas(width="300", height="250", highlightthickness=0,bg="white")
+        self.question_text=self.canvas.create_text(150,125,text="Some text",fill=THEME_COLOR,
+                                                   font=FONT,width=200)
 
-    canvas.itemconfig(text_word,text=selected["French"],fill="black")
-    canvas.itemconfig(text_lang,text="French",fill="black")
-    canvas.itemconfig(photo,image=cardfront)
-    flip_timer=window.after(3000,change_image)
+        self.canvas.grid(row=1, column=0, columnspan=2,pady=50)
 
-def change_image():
-    canvas.itemconfig(photo, image=card_open)
-    canvas.itemconfig(text_lang,text="English",fill="white")
-    canvas.itemconfig(text_word, text=selected['English'],fill="white")
-
-def is_known():
-    generate_word.remove(selected)
-    print(len(generate_word))
-    data=pd.DataFrame(generate_word)
-    data.to_csv("data/words_to_learn.csv",index=False);
-    randomwords()
-
-window = Tk()
-window.title("FlashCard App")
-window.config(bg=BACKGROUND_COLOR,padx=40,pady=40)
-flip_timer=window.after(3000, change_image)
-#delay
+        self.tick_image = PhotoImage(file="images/true.png")
+        self.correct = Button(image=self.tick_image, highlightthickness=0,command=self.trueAnswered)
+        self.correct.grid(column=0, row=2,pady=50)
+        cross_image = PhotoImage(file="images/false.png")
+        self.wrong = Button(image=cross_image, highlightthickness=0,command=self.falseAnswered)
+        self.wrong.grid(row=2, column=1,pady=50)
+        self.get_next()
+        self.window.mainloop()
 
 
+    def set(self):
+        self.window.title("Quiz App")
+        self.window.config(bg=THEME_COLOR,padx=20, pady=20)
+
+    def get_next(self):
+        if self.quiz.still_has_questions():
+            self.canvas.config(bg="white")
+            self.qtext=self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text,text=self.qtext)
 
 
+    def falseAnswered(self):
+        is_right = self.quiz.check_answer("false")
+        print(is_right)
+        self.feedback(is_right)
+        #self.get_next()
 
-canvas =Canvas(width="800",height="526",highlightthickness=0,bg=BACKGROUND_COLOR)
-card_open=PhotoImage(file="images/card_back.png")
-cardfront=PhotoImage(file="images/card_front.png")
-photo=canvas.create_image(400,263,image=cardfront)
-text_lang=canvas.create_text(400, 150, text='French',font="Arial 40 italic")
-text_word=canvas.create_text(400,263,text="",font="Arial 60 bold")
-canvas.grid(row=0,column=0,columnspan=2)
-
-#Buttons
-tick_image = PhotoImage(file="images/right.png")
-correct = Button(image=tick_image, highlightthickness=0,command=is_known)
-correct.grid(column=0,row=1)
-cross_image = PhotoImage(file="images/wrong.png")
-wrong = Button(image=cross_image, highlightthickness=0,command=randomwords)
-wrong.grid(row=1,column=1)
-randomwords()
+    def trueAnswered(self):
+        is_right=self.quiz.check_answer("True")
+        print(is_right)
+        self.feedback(is_right)
 
 
-window.mainloop()
+    # def check(self,is_right):
+    #     if is_right==True:
+    #         self.score+=1
+    #         print(self.score)
+    #
+    #         self.feedback()
+    #     else:
+    #         self.feedback(False)
+
+    def feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+            self.label.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000,self.get_next)
+
+    # def chosenCategory(self):
+    #     choice=self.menu.get()
+    #     if(choice=="Entertainment"):
+    #         id=
